d4d6f28ee7cfb79b373bd7a719332145b9dfd66e
Alvaro <alvaroarellano2002.124@gmail.com>
2024-02-13 23:43:11 +0100

    Acaba reciclerview de Podcast

---
+++ app/src/main/java/com/dabellan/mibibliotecamusical/Podcast.kt
@@ -0,0 +1,9 @@
+package com.dabellan.mibibliotecamusical
+
+data class Podcast(
+    var id: Long = 0,
+    var titulo: String,
+    var imagen: String,
+    var descripcion: String
+    //var anyo: Date
+)
